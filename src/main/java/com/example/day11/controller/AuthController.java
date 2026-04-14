853f06aaaefa9579147977c3c63ef07a7e15c7f4
sathvikinguva <sathvikinguva@gmail.com>
2026-04-14 15:21:58 +0530

    solved

--- src/main/java/com/example/day11/controller/AuthController.java
+++ src/main/java/com/example/day11/controller/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.boot.jackson.autoconfigure.JacksonProperties.Json;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.day11.dto.JsonResponse;
 import com.example.day11.dto.UserCreateRequest;
